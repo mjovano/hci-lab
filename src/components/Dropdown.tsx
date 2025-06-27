140a1d0a9b204ce5cf88126f5d5cb6615b951b6d
mjovano <127967109+mjovano@users.noreply.github.com>
2025-06-27 07:34:37 +0200

    v-fin

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -22,7 +22,7 @@ export default function DropdownMenu() {
         <div className="py-1">
           <MenuItem>
             <a
-              href="/shop?type=tents"
+              href="/shop?type=tent"
               className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
             >
               Tents
